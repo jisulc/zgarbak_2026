f19c26a861b5bcc5f36fa2fbe264956cda0d995d
jisulc <jisulc@email.cz>
2026-01-03 20:30:31 +0100

    win

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const heading = document.getElementById("text");
 const speedOr = document.getElementById("speed");
+const ageInput = document.getElementById("vek");
+const adultText = document.getElementById("dospelec");
+const nameInput = document.getElementById("jmeno");
 
 const text = "Toto je největší Jirkův Zgarbák!"
 
@@ -7,6 +10,31 @@ let idletter = 1;
 
 let delay = 500 / speedOr.value;
 
+//počet dosažených bodů
+
+let student1 = 125;
+let student2 = 92;
+
+//typ člověka
+let adult = Boolean;
+
+// jména studentů
+let student1Name = nameInput.value;
+let student2Name = "Pepa";
+
+//maximální počet bodů
+const maxPoints = 200;
+
+//výpočet úspěšnosti v %
+let result1 = (student1 / maxPoints) * 100;
+let result2 = (student2 / maxPoints) * 100;
+
+//výsledky do konzole
+
+function vypisToConsole()   {
+    console.log(`${student1Name} má úspěšnost ${result1}%`);    
+    console.log(student2Name+ " má úspěšnost "+ result2 + "%");
+}
 
 function printText() {
     heading.innerText = text.slice(0, idletter);
@@ -17,7 +45,7 @@ function printText() {
     //vyresetuje pořadí písmen
 
     if (idletter > text.length) {
-        idletter = 1;
+        idletter = 1; 
     }
 
 }
@@ -28,11 +56,35 @@ function pozdrav() {
 
 }
 
-
 speedOr.addEventListener("input", function(event) {
     console.log(event.target.value);
     delay = 500 / event.target.value;
 });
+
+ageInput.addEventListener("input", function(event) {
+    console.log(event.target.value);
+    if (event.target.value >= 18) {
+        adult = true;
+        adultText.innerText = "Jsi dospělý/á.";
+    } else {
+        adult = false;
+        adultText.innerText = "Je ti méně než 18 let.";
+    }   
+});
+
+
+nameInput.addEventListener("change", function(event) {
+ 
+    student1Name = event.target.value;
+    vypisToConsole();   
+
+});
+
+let naDruhou = function(x) {
+    return x * x;
+}
+
+console.log(naDruhou(5));
 
 
 pozdrav();
